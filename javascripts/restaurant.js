9f6f02a8393f3024ea9246ded6fe3d29f706361b
Eva <blue800415@gmail.com>
2014-06-27 19:20:15 +0800

    record score

--- javascripts/restaurant.js
+++ javascripts/restaurant.js
@@ -20,11 +20,11 @@ function Get_Restaurant(content){
 	var data;	
 	
 	$.support.cors = true;
-    $.get('test.txt', function(data) {
-		var Food = data.split("---------------------");		
+    $.get('data/result.txt', function(data) {
+		var Food = data.split("--------------------");		
 
-		var info = Food[ID-1].split("\n");
-		if(ID>1)
+		var info = Food[ID].split("\n");
+		if(ID>0)
 			info.shift();
 		if(content == 1)
 			Restaurant_content(info);
@@ -35,49 +35,57 @@ function Get_Restaurant(content){
 
 function Scoring_content(info){
 	var content;
-
+	var fcount = (info.length-6)/2;
 	content="對於該家餐廳，您認為此店家<br/>";
-	content=content + "<form>";
-	content=content + "<input type=\"radio\" name=\"All_score\" value=\"1\"> 極差";
-	content=content + "<input type=\"radio\" name=\"All_score\" value=\"2\"> 差強人意";
-	content=content + "<input type=\"radio\" name=\"All_score\" value=\"3\"> 普通";
-	content=content + "<input type=\"radio\" name=\"All_score\" value=\"4\"> 不錯";
-	content=content + "<input type=\"radio\" name=\"All_score\" value=\"5\"> 很棒!";
-	content=content + "</form><br/>";
+	content=content + "<form action=\"score_submit.php?id=" + Get_para("id") + "&fcount=" + fcount + "\" method=\"post\">";
+	content=content + "<input type=\"radio\" name=\"All_score\" value = \"1\"> 極差　";
+	content=content + "<input type=\"radio\" name=\"All_score\" value = \"2\"> 欠佳　";
+	content=content + "<input type=\"radio\" name=\"All_score\" value = \"3\"> 普通　";
+	content=content + "<input type=\"radio\" name=\"All_score\" value = \"4\"> 不錯　";
+	content=content + "<input type=\"radio\" name=\"All_score\" value = \"5\"> 很棒";
+	content=content + "<br/>";
 	
 	for(i=5;i<info.length-1;i=i+2){
 		content = content + "<br/>";
-		content = content + "對於該家餐廳，您認為<b>" + info[i] + "</b>";
-		content = content + "<form>";
-		content = content + "<input type=\"radio\" name=\"Food_"+ (i-4) +"\" value=\"1\"> 極差";
-		content = content + "<input type=\"radio\" name=\"Food_"+ (i-4) +"\" value=\"2\"> 差強人意";
-		content = content + "<input type=\"radio\" name=\"Food_"+ (i-4) +"\" value=\"3\"> 普通";
-		content = content + "<input type=\"radio\" name=\"Food_"+ (i-4) +"\" value=\"4\"> 不錯";
-		content = content + "<input type=\"radio\" name=\"Food_"+ (i-4) +"\" value=\"5\"> 很棒!";
-		content = content + "</form><br/>";				
+		content = content + "對於該家餐廳，您認為<b>" + info[i] + "</b><br/>";
+		content = content + "<input type=\"radio\" name=\"Food_"+ (i-5)/2 +"\" value = \"1\"> 極差　";
+		content = content + "<input type=\"radio\" name=\"Food_"+ (i-5)/2 +"\" value = \"2\"> 欠佳　";
+		content = content + "<input type=\"radio\" name=\"Food_"+ (i-5)/2 +"\" value = \"3\"> 普通　";
+		content = content + "<input type=\"radio\" name=\"Food_"+ (i-5)/2 +"\" value = \"4\"> 不錯　";
+		content = content + "<input type=\"radio\" name=\"Food_"+ (i-5)/2 +"\" value = \"5\"> 很棒";
+		content = content + "<br/>";				
 	}
 
-	content=content +"<br/>對於該家餐廳，您認為<input type=\"text\" name=\"else_name\"><br/>";
-	content=content + "<form>";
-	content=content + "<input type=\"radio\" name=\"else\" value=\"1\"> 極差";
-	content=content + "<input type=\"radio\" name=\"else\" value=\"2\"> 差強人意";
-	content=content + "<input type=\"radio\" name=\"else\" value=\"3\"> 普通";
-	content=content + "<input type=\"radio\" name=\"else\" value=\"4\"> 不錯";
-	content=content + "<input type=\"radio\" name=\"else\" value=\"5\"> 很棒!";
-	content=content + "</form><br/><center><input type=\"button\" value=\"送出\"></br></br></center>";
+	content=content + "<br/><center><input type=\"submit\" value=\"送出\"></form></br></br></center>";
 	
 	document.getElementById("name").innerHTML="<a href=\"restaurant.html?id=" + Get_para("id")+ "\">" + "<image src=\"image/back.png\" width=\"40px\">" + "</a>";
 	document.getElementById("score").innerHTML=content;	
 }
 
 function Restaurant_content(info){
-
-	document.getElementById("name").innerHTML=info[0]+"<a href=\"restaurant_score.html?id=" + Get_para("id")+ "\"><image src=\"image/score.png\" width = \"60px\"></a>";
+	
+	
+	document.getElementById("name").innerHTML= info[0] + "<a href=\"restaurant_score.html?id=" + Get_para("id")+ "\"><image src=\"image/score.png\" width = \"60px\"></a>";
 	document.getElementById("Address").innerHTML=info[2];
 	
 	var foods="";
 	for(i=5;i<info.length-1;i=i+2){
-		foods = foods+"<tr><td>" + info[i] + "</td><td>: ★★★☆☆</td></tr>";					
+		foods = foods+"<tr><td>" + info[i] + "</td><td>: " + Show_start(info[i+1].split(" ")[1]) + "</td></tr>";					
 	}
 	document.getElementById("Foods").innerHTML = foods;
+}
+
+function Show_start(score){
+	var content = "";
+	if(score <= 1 )
+		content = "★☆☆☆☆";
+	else if(score <= 2 )
+		content = "★★☆☆☆";
+	else if(score <= 3)
+		content = "★★★☆☆";
+	else if(score <= 4)
+		content = "★★★★☆";
+	else
+		content = "★★★★★";
+	return content;
 }
---
+++ score_submit.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Information</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+	<script type="text/javascript" src="javascripts/restaurant.js"></script>
+	<script type="text/javascript" src="javascripts/jquery-1.4.3.js"></script>
+	
+</head>
+<body>
+	<center><table height = "280px">感謝您的意見!</table></center>
+<?	
+	$id = $_GET["id"];
+	$fcount = $_GET["fcount"];
+	$flag = 0;
+	
+	$Upload_folder="data/Feedback/".$id."/";
+	if (!is_dir("data/Feedback/".$id)){
+	  mkdir($Upload_folder);
+	}
+	
+	$All_score = $_POST["All_score"];
+	if($All_score != ""){
+		$flag = 1;
+		$file = fopen("data/Feedback/".$id."/All_score.txt","a+");
+		fwrite($file,$All_score." ");
+		fclose($file);
+	}
+	
+	for($i=0 ; $i < $fcount ; $i++){
+		$Food_score = $_POST["Food_".$i];
+		if($Food_score != ""){
+			$flag = 1;
+			$file_food = fopen("data/Feedback/".$id."/".$i.".txt","a+");
+			fwrite($file_food,$Food_score." ");
+			fclose($file_food);
+		}		
+	}
+?>
+</body>
+</html>
